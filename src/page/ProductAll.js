58149c7840b872ce88c8db95a227f3b32b726303
hyunju960429 <145514544+hyunju960429@users.noreply.github.com>
2024-01-18 11:11:22 +0900

    Update ProductAll.js

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -16,7 +16,7 @@ const ProductAll = () => {
     console.log(searchQuery);
 
     //let url = `http://localhost:3004/products?q=${searchQuery}`;
-    let url = `https://my-json-server.typicode.com/<your-username>/<your-repo>/products?q=${searchQuery}`;
+    let url = `https://my-json-server.typicode.com/hyunju960429/shopping/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     //console.log(data);
